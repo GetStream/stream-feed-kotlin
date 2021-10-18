70e07c36ab5792c23e958630817cf45f3ba38a7e
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-10-18 14:18:59 +0200

    Add buildSrc

---
+++ buildSrc/src/main/kotlin/io/getstream/feed/Configuration.kt
@@ -0,0 +1,13 @@
+package io.getstream.feed
+
+object Configuration {
+    const val compileSdk = 31
+    const val targetSdk = 30
+    const val minSdk = 21
+    const val majorVersion = 0
+    const val minorVersion = 0
+    const val patchVersion = 1
+    const val versionName = "$majorVersion.$minorVersion.$patchVersion"
+    const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
+    const val artifactGroup = "io.getstream"
+}
---
+++ buildSrc/src/main/kotlin/io/getstream/feed/Dependencies.kt
@@ -0,0 +1,19 @@
+package io.getstream.feed
+
+object Versions {
+    internal const val ANDROID_GRADLE_PLUGIN = "7.0.3"
+    internal const val GRADLE_VERSIONS_PLUGIN = "0.39.0"
+    internal const val KOTLIN = "1.5.31"
+}
+
+object Dependencies {
+    const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.ANDROID_GRADLE_PLUGIN}"
+    const val gradleVersionsPlugin = "com.github.ben-manes:gradle-versions-plugin:${Versions.GRADLE_VERSIONS_PLUGIN}"
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN}"
+
+    @JvmStatic
+    fun isNonStable(version: String): Boolean = isStable(version).not()
+
+    @JvmStatic
+    fun isStable(version: String): Boolean = ("^[0-9.]+$").toRegex().matches(version)
+}
