9656fbb719064dc9e6961054a5c7d7e1587e4533
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-10-18 18:30:03 +0200

    Add Testing Frameworks

--- buildSrc/src/main/kotlin/io/getstream/feed/Dependencies.kt
+++ buildSrc/src/main/kotlin/io/getstream/feed/Dependencies.kt
@@ -2,20 +2,33 @@ package io.getstream.feed
 
 object Versions {
     internal const val ANDROID_GRADLE_PLUGIN = "7.0.3"
-    internal const val DOKKA = "1.5.0"
+    internal const val ANDROID_JUNIT5_GRADLE_PLUGIN = "1.8.0.0"
+    internal const val DOKKA = "1.5.31"
     internal const val GRADLE_NEXUS_PUBLISH_PLUGIN = "1.1.0"
     internal const val GRADLE_VERSIONS_PLUGIN = "0.39.0"
+    internal const val JUNIT5 = "5.8.1"
+    internal const val KLUENT = "1.68"
     internal const val KOTLIN = "1.5.31"
     internal const val KTLINT_PLUGIN = "10.2.0"
+    internal const val MOCKITO = "4.0.0"
+    internal const val MOCKITO_KOTLIN = "2.2.0"
 }
 
 object Dependencies {
     const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.ANDROID_GRADLE_PLUGIN}"
+    const val androidJunit5GradlePlugin =
+        "de.mannodermaus.gradle.plugins:android-junit5:${Versions.ANDROID_JUNIT5_GRADLE_PLUGIN}"
     const val dokka = "org.jetbrains.dokka:dokka-gradle-plugin:${Versions.DOKKA}"
     const val gradleNexusPublishPlugin = "io.github.gradle-nexus:publish-plugin:${Versions.GRADLE_NEXUS_PUBLISH_PLUGIN}"
     const val gradleVersionsPlugin = "com.github.ben-manes:gradle-versions-plugin:${Versions.GRADLE_VERSIONS_PLUGIN}"
+    const val junitJupiterApi = "org.junit.jupiter:junit-jupiter-api:${Versions.JUNIT5}"
+    const val junitJupiterParams = "org.junit.jupiter:junit-jupiter-params:${Versions.JUNIT5}"
+    const val junitJupiterEngine = "org.junit.jupiter:junit-jupiter-engine:${Versions.JUNIT5}"
+    const val kluent = "org.amshove.kluent:kluent:${Versions.KLUENT}"
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN}"
     const val ktlintPlugin = "org.jlleitschuh.gradle:ktlint-gradle:${Versions.KTLINT_PLUGIN}"
+    const val mockito = "org.mockito:mockito-core:${Versions.MOCKITO}"
+    const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.MOCKITO_KOTLIN}"
 
     @JvmStatic
     fun isNonStable(version: String): Boolean = isStable(version).not()
---
+++ stream-feed-client/src/test/kotlin/client/StreamClientTest.kt
@@ -0,0 +1,12 @@
+package client
+
+import org.amshove.kluent.`should be equal to`
+import org.junit.jupiter.api.Test
+
+internal class StreamClientTest {
+
+    @Test
+    fun emptyTest() {
+        1 + 1 `should be equal to` 2
+    }
+}
