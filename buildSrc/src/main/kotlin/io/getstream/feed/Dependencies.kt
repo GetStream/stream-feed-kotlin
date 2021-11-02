e2e5c167ed6c06c61fe1649930385f8e5a3c9ad1
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-11-02 11:49:26 +0100

    Add Coroutines dependency

--- buildSrc/src/main/kotlin/io/getstream/feed/Dependencies.kt
+++ buildSrc/src/main/kotlin/io/getstream/feed/Dependencies.kt
@@ -3,6 +3,7 @@ package io.getstream.feed
 object Versions {
     internal const val ANDROID_GRADLE_PLUGIN = "7.0.3"
     internal const val ANDROID_JUNIT5_GRADLE_PLUGIN = "1.8.0.0"
+    internal const val COROUTINES = "1.5.2"
     internal const val DOKKA = "1.5.31"
     internal const val GRADLE_NEXUS_PUBLISH_PLUGIN = "1.1.0"
     internal const val GRADLE_VERSIONS_PLUGIN = "0.39.0"
@@ -20,6 +21,8 @@ object Dependencies {
     const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.ANDROID_GRADLE_PLUGIN}"
     const val androidJunit5GradlePlugin =
         "de.mannodermaus.gradle.plugins:android-junit5:${Versions.ANDROID_JUNIT5_GRADLE_PLUGIN}"
+    const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.COROUTINES}"
+    const val coroutinesTest = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.COROUTINES}"
     const val dokka = "org.jetbrains.dokka:dokka-gradle-plugin:${Versions.DOKKA}"
     const val gradleNexusPublishPlugin = "io.github.gradle-nexus:publish-plugin:${Versions.GRADLE_NEXUS_PUBLISH_PLUGIN}"
     const val gradleVersionsPlugin = "com.github.ben-manes:gradle-versions-plugin:${Versions.GRADLE_VERSIONS_PLUGIN}"
