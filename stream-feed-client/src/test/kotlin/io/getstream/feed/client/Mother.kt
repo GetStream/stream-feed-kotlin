ddc31c1fdb6383c8d801d7330aa8487a9807ebbf
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-11-08 18:14:42 +0100

    Add Mothers for Request Objects

--- stream-feed-client/src/test/kotlin/io/getstream/feed/client/Mother.kt
+++ stream-feed-client/src/test/kotlin/io/getstream/feed/client/Mother.kt
@@ -1,23 +1,32 @@
 package io.getstream.feed.client
 
+import io.getstream.feed.client.internal.api.models.ActivitiesRequest
 import io.getstream.feed.client.internal.api.models.ActorDataDto
 import io.getstream.feed.client.internal.api.models.ActorDto
 import io.getstream.feed.client.internal.api.models.CustomExtraDTOs
 import io.getstream.feed.client.internal.api.models.DownstreamActivityDto
 import io.getstream.feed.client.internal.api.models.DownstreamEnrichActivityDto
+import io.getstream.feed.client.internal.api.models.UpdateActivitiesRequest
+import io.getstream.feed.client.internal.api.models.UpdateActivityByForeignIdRequest
+import io.getstream.feed.client.internal.api.models.UpdateActivityByIdRequest
+import io.getstream.feed.client.internal.api.models.UpdateActivityRequest
 import io.getstream.feed.client.internal.api.models.UpstreamActivityDto
+import io.getstream.feed.client.internal.api.models.UpstreamActivitySealedDto
 import io.getstream.feed.client.internal.api.models.UpstreamEnrichActivityDto
 import kotlin.random.Random
 
 internal object Mother {
     private val random = Random(System.currentTimeMillis())
     private val charPool: CharArray = (('a'..'z') + ('A'..'Z') + ('0'..'9')).toCharArray()
-
+    fun positiveRandomInt(maxInt: Int = Int.MAX_VALUE - 1): Int =
+        random.nextInt(0, maxInt + 1)
     fun randomString(size: Int = 20): String = buildString(capacity = size) {
         repeat(size) {
             append(charPool.random())
         }
     }
+
+    fun <T> oneOf(vararg randomGen: () -> T): T = randomGen.random().invoke()
 
     fun <T> randomOrNull(randomGen: () -> T): T? = random.nextBoolean().let {
         when (it) {
@@ -28,13 +37,13 @@ internal object Mother {
 
     fun randomStringOrNull(): String? = randomOrNull(::randomString)
 
-    fun <T> randomListOfOrNull(size: Int = 20, randomGen: () -> T): List<T>? =
+    fun <T> randomListOfOrNull(size: Int = positiveRandomInt(20), randomGen: () -> T): List<T>? =
         randomOrNull {
             randomListOf {
                 randomGen()
             }
         }
-    fun <T> randomListOf(size: Int = 20, randomGen: () -> T): List<T> = List(size = size) { randomGen() }
+    fun <T> randomListOf(size: Int = positiveRandomInt(20), randomGen: () -> T): List<T> = List(size = size) { randomGen() }
 
     fun randomActorDto(
         id: String = randomString(),
@@ -100,6 +109,52 @@ internal object Mother {
         extraData: Map<String, Any> = emptyMap()
     ): DownstreamEnrichActivityDto =
         DownstreamEnrichActivityDto(id, actor, objectProperty, verb, target, time, to, foreignId).withExtras(extraData)
+
+    fun randomUpdateActivityByIdRequest(
+        id: String = randomString(),
+        set: Map<String, Any> = emptyMap(),
+        unset: List<String> = randomListOf { randomString() },
+    ): UpdateActivityByIdRequest =
+        UpdateActivityByIdRequest(id, set, unset)
+
+    fun randomUpdateActivityByForeignIdRequest(
+        foreignId: String = randomString(),
+        time: String = randomString(),
+        set: Map<String, Any> = emptyMap(),
+        unset: List<String> = randomListOf { randomString() },
+    ): UpdateActivityByForeignIdRequest =
+        UpdateActivityByForeignIdRequest(foreignId, time, set, unset)
+
+    fun randomUpdateActivityRequest(
+        set: Map<String, Any> = emptyMap(),
+        unset: List<String> = randomListOf { randomString() },
+    ): UpdateActivityRequest = oneOf(
+        {
+            randomUpdateActivityByIdRequest(
+                set = set,
+                unset = unset,
+            )
+        },
+        {
+            randomUpdateActivityByForeignIdRequest(
+                set = set,
+                unset = unset,
+            )
+        }
+    )
+
+    fun randomUpstreamActivitySealedDto(): UpstreamActivitySealedDto =
+        oneOf(::randomUpstreamActivityDto, ::randomUpstreamEnrichActivitySDto)
+
+    fun randomUpdateActivitiesRequest(
+        updates: List<UpdateActivityRequest> = randomListOf { randomUpdateActivityRequest() }
+    ): UpdateActivitiesRequest =
+        UpdateActivitiesRequest(updates)
+
+    fun randomActivitiesRequest(
+        activities: List<UpstreamActivitySealedDto> = randomListOf { randomUpstreamActivitySealedDto() }
+    ): ActivitiesRequest =
+        ActivitiesRequest(activities)
 
     private fun <T : CustomExtraDTOs> T.withExtras(extraData: Map<String, Any>) = apply { this.extraData.putAll(extraData) }
 }
