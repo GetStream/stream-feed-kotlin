c9ac7eb22013cb5b55a46fa33bf7d604651fa59e
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-11-12 16:28:22 +0100

    Create GetActivitiesParams

---
+++ stream-feed-client/src/main/java/io/getstream/feed/client/GetActivitiesParams.kt
@@ -0,0 +1,19 @@
+package io.getstream.feed.client
+
+class GetActivitiesParams {
+    var limit: Int = 25
+    var offset: Int? = null
+    var idGreaterThan: String? = null
+    var idGreaterThanOrEqual: String? = null
+    var idSmallerThan: String? = null
+    var idSmallerThanOrEqual: String? = null
+    var withOwnReactions: Boolean = false
+    var withReactionCounts: Boolean = false
+    internal var withRecentReactions: Boolean = false
+    internal var recentReactionsLimit: Int? = null
+
+    fun withRecentReactions(limit: Int? = null) = apply {
+        withRecentReactions = true
+        recentReactionsLimit = limit
+    }
+}
