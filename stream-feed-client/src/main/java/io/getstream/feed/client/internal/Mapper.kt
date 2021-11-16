e46b823ba960a9d7f9bfc17588bd8e03c870cd00
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-11-16 15:59:19 +0100

    Add mappers to obtain either with errors

--- stream-feed-client/src/main/java/io/getstream/feed/client/internal/Mapper.kt
+++ stream-feed-client/src/main/java/io/getstream/feed/client/internal/Mapper.kt
@@ -8,11 +8,16 @@ import io.getstream.feed.client.Actor
 import io.getstream.feed.client.EnrichActivity
 import io.getstream.feed.client.FeedActivity
 import io.getstream.feed.client.FeedID
+import io.getstream.feed.client.NetworkError
+import io.getstream.feed.client.StreamAPIError
+import io.getstream.feed.client.StreamError
+import io.getstream.feed.client.internal.api.adapters.FeedMoshiConverterFactory
 import io.getstream.feed.client.internal.api.models.ActivitiesResponse
 import io.getstream.feed.client.internal.api.models.ActorDto
 import io.getstream.feed.client.internal.api.models.DownstreamActivityDto
 import io.getstream.feed.client.internal.api.models.DownstreamActivitySealedDto
 import io.getstream.feed.client.internal.api.models.DownstreamEnrichActivityDto
+import io.getstream.feed.client.internal.api.models.ErrorResponse
 import retrofit2.Response
 
 internal fun ActorDto.toDomain(): Actor =
@@ -58,7 +63,25 @@ private fun String.toFeedID(): FeedID = split(":").let { FeedID(it[0], it[1]) }
 internal fun ActivitiesResponse.toDomain(): List<FeedActivity> =
     activities.map { it.toDomain() }
 
-internal fun <T> Response<T>.obtainEntity(): Either<Unit, T> = when (isSuccessful) {
-    true -> body().rightIfNotNull { }
-    false -> Unit.left()
+internal fun <T> Response<T>.obtainEntity(): Either<StreamError, T> = when (isSuccessful) {
+    true -> body().rightIfNotNull { toError() }
+    false -> toError().left()
 }
+
+private fun Response<*>.toError(): StreamError = this.errorBody()
+    ?.string()
+    ?.let {
+        try {
+            FeedMoshiConverterFactory.moshi.adapter(ErrorResponse::class.java).fromJson(it)?.toDomain()
+        } catch (_: Exception) {
+            null
+        } ?: NetworkError(it)
+    } ?: NetworkError(this.toString())
+
+private fun ErrorResponse.toDomain(): StreamAPIError = StreamAPIError(
+    detail = detail,
+    statusCode = statusCode,
+    errorCode = errorCode,
+    error = error,
+    moreInfo = moreInfo,
+)
