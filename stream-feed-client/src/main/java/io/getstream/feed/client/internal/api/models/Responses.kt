20f51879800ee404d48d07973270c2c858a579fb
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-11-16 15:55:21 +0100

    Add ErrorResponse

--- stream-feed-client/src/main/java/io/getstream/feed/client/internal/api/models/Responses.kt
+++ stream-feed-client/src/main/java/io/getstream/feed/client/internal/api/models/Responses.kt
@@ -12,3 +12,12 @@ internal data class ActivitiesResponse(
 internal data class CreateActivitiesResponse(
     @Json(name = "activities") val activities: List<DownstreamActivitySealedDto>,
 )
+
+@JsonClass(generateAdapter = true)
+internal data class ErrorResponse(
+    @Json(name = "detail") val detail: String,
+    @Json(name = "status_code") val statusCode: Int,
+    @Json(name = "code") val errorCode: Int,
+    @Json(name = "exception") val error: String,
+    @Json(name = "more_info") val moreInfo: String,
+)
