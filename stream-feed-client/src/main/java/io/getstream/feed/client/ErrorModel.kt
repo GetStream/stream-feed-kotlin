2da261da16aac6d970a1436fc989344baf3abc12
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-11-16 16:00:26 +0100

    Create ErrorModels

---
+++ stream-feed-client/src/main/java/io/getstream/feed/client/ErrorModel.kt
@@ -0,0 +1,16 @@
+package io.getstream.feed.client
+
+sealed class StreamError
+data class StreamAPIError(
+    val detail: String,
+    val statusCode: Int,
+    val errorCode: Int,
+    val error: String,
+    val moreInfo: String,
+) : StreamError()
+data class NetworkError(val message: String) : StreamError()
+sealed class ParamError : StreamError() {
+    abstract val message: String
+}
+data class NegativeParamError(override val message: String) : ParamError()
+data class IncompatibleParamsError(override val message: String) : ParamError()
