05797ebb34af417fe1e1f96cb1a2ee2de3676075
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-11-17 13:55:12 +0100

    Create more mother methods

--- stream-feed-client/src/test/kotlin/io/getstream/feed/client/Mother.kt
+++ stream-feed-client/src/test/kotlin/io/getstream/feed/client/Mother.kt
@@ -15,8 +15,8 @@ import kotlin.random.Random
 internal object Mother {
     private val random = Random(System.currentTimeMillis())
     private val charPool: CharArray = (('a'..'z') + ('A'..'Z') + ('0'..'9')).toCharArray()
-    fun positiveRandomInt(maxInt: Int = Int.MAX_VALUE - 1): Int =
-        random.nextInt(maxInt + 1)
+    fun positiveRandomInt(maxInt: Int = Int.MAX_VALUE - 2): Int =
+        random.nextInt(maxInt + 2) + 1
     fun negativeRandomInt(minInt: Int = Int.MIN_VALUE): Int =
         random.nextInt(minInt, 0)
     fun randomString(size: Int = 20): String = buildString(capacity = size) {
@@ -95,6 +95,46 @@ internal object Mother {
         extraData: MutableMap<String, Any> = mutableMapOf()
     ): DownstreamEnrichActivityDto =
         DownstreamEnrichActivityDto(id, actor, objectProperty, verb, target, time, to, foreignId, extraData)
+
+    fun randomActor(
+        id: String = randomString(),
+        handle: String = randomString(),
+        name: String = randomString(),
+        profileImage: String = randomString(),
+        extraData: Map<String, Any> = emptyMap()
+    ): Actor =
+        Actor(id, handle, name, profileImage, extraData)
+
+    fun randomActivity(
+        id: String = randomString(),
+        actor: String = randomString(),
+        `object`: String = randomString(),
+        verb: String = randomString(),
+        to: List<FeedID> = emptyList(),
+        time: String = randomString(),
+        foreignId: String? = randomOrNull { randomString() },
+        extraData: Map<String, Any> = emptyMap(),
+    ): Activity =
+        Activity(id, actor, `object`, verb, to, time, foreignId, extraData)
+
+    fun randomEnrichActivity(
+        id: String = randomString(),
+        actor: Actor = randomActor(),
+        `object`: String = randomString(),
+        verb: String = randomString(),
+        to: List<FeedID> = emptyList(),
+        time: String = randomString(),
+        foreignId: String? = randomOrNull { randomString() },
+        extraData: Map<String, Any> = emptyMap(),
+    ): EnrichActivity =
+        EnrichActivity(id, actor, `object`, verb, to, time, foreignId, extraData)
+
+    fun randomFeedActivity(): FeedActivity =
+        oneOf(::randomActivity, ::randomEnrichActivity)
+
+    fun randomListOfFeedActivity(): List<FeedActivity> = randomListOf { randomFeedActivity() }
+    fun randomListOfActivity(): List<Activity> = randomListOf { randomActivity() }
+    fun randomListOfEnrichActivity(): List<EnrichActivity> = randomListOf { randomEnrichActivity() }
 
     fun randomUpdateActivityByIdRequest(
         id: String = randomString(),
