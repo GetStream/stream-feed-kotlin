ee4689cc1359cadbcc990f17757ad204c7af82de
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-11-17 13:15:33 +0100

    Create mapper for entities to create new activities

--- stream-feed-client/src/main/java/io/getstream/feed/client/internal/Mapper.kt
+++ stream-feed-client/src/main/java/io/getstream/feed/client/internal/Mapper.kt
@@ -14,10 +14,12 @@ import io.getstream.feed.client.StreamError
 import io.getstream.feed.client.internal.api.adapters.FeedMoshiConverterFactory
 import io.getstream.feed.client.internal.api.models.ActivitiesResponse
 import io.getstream.feed.client.internal.api.models.ActorDto
+import io.getstream.feed.client.internal.api.models.CreateActivitiesResponse
 import io.getstream.feed.client.internal.api.models.DownstreamActivityDto
 import io.getstream.feed.client.internal.api.models.DownstreamActivitySealedDto
 import io.getstream.feed.client.internal.api.models.DownstreamEnrichActivityDto
 import io.getstream.feed.client.internal.api.models.ErrorResponse
+import io.getstream.feed.client.internal.api.models.UpstreamActivityDto
 import retrofit2.Response
 
 internal fun ActorDto.toDomain(): Actor =
@@ -59,9 +61,42 @@ internal fun DownstreamEnrichActivityDto.toDomain(): EnrichActivity =
     )
 
 private fun String.toFeedID(): FeedID = split(":").let { FeedID(it[0], it[1]) }
+private fun FeedID.toStringFeedID(): String = "$slug:$userID"
 
 internal fun ActivitiesResponse.toDomain(): List<FeedActivity> =
     activities.map { it.toDomain() }
+
+internal fun FeedActivity.toDTO(): UpstreamActivityDto = when (this) {
+    is Activity -> toDTO()
+    is EnrichActivity -> toDTO()
+}
+
+internal fun Activity.toDTO(): UpstreamActivityDto =
+    UpstreamActivityDto(
+        actor = actor,
+        objectProperty = `object`,
+        verb = verb,
+        target = null,
+        time = time.takeUnless(String::isBlank),
+        to = to.map(FeedID::toStringFeedID).takeUnless(List<String>::isEmpty),
+        foreignId = foreignId,
+        extraData = extraData.toMutableMap(),
+    )
+
+internal fun EnrichActivity.toDTO(): UpstreamActivityDto =
+    UpstreamActivityDto(
+        actor = actor.id,
+        objectProperty = `object`,
+        verb = verb,
+        target = null,
+        time = time.takeUnless(String::isBlank),
+        to = to.map(FeedID::toStringFeedID).takeUnless(List<String>::isEmpty),
+        foreignId = foreignId,
+        extraData = extraData.toMutableMap(),
+    )
+
+internal fun CreateActivitiesResponse.toDomain(): List<FeedActivity> =
+    activities.map(DownstreamActivitySealedDto::toDomain)
 
 internal fun <T> Response<T>.obtainEntity(): Either<StreamError, T> = when (isSuccessful) {
     true -> body().rightIfNotNull { toError() }
