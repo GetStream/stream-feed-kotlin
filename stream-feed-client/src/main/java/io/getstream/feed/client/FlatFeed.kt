02996b0c1ada0d23f891764734c28fbdf98f98ee
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-11-17 13:53:57 +0100

    Create a method to add activities

--- stream-feed-client/src/main/java/io/getstream/feed/client/FlatFeed.kt
+++ stream-feed-client/src/main/java/io/getstream/feed/client/FlatFeed.kt
@@ -3,8 +3,11 @@ package io.getstream.feed.client
 import arrow.core.Either
 import arrow.core.computations.either
 import io.getstream.feed.client.internal.api.FeedApi
+import io.getstream.feed.client.internal.api.models.ActivitiesRequest
 import io.getstream.feed.client.internal.api.models.ActivitiesResponse
+import io.getstream.feed.client.internal.api.models.CreateActivitiesResponse
 import io.getstream.feed.client.internal.obtainEntity
+import io.getstream.feed.client.internal.toDTO
 import io.getstream.feed.client.internal.toDomain
 import io.getstream.feed.client.internal.validate
 
@@ -33,4 +36,23 @@ class FlatFeed internal constructor(
                 .map(ActivitiesResponse::toDomain)
                 .bind()
         }
+
+    suspend fun addActivities(activities: List<FeedActivity>): Either<StreamError, List<FeedActivity>> = either {
+        val activitiesRequest = ActivitiesRequest(
+            activities = activities
+                .validate()
+                .bind()
+                .map(FeedActivity::toDTO)
+        )
+        feedApi.sendActivities(
+            slug = feedID.slug,
+            id = feedID.userID,
+            activities = activitiesRequest
+        ).obtainEntity()
+            .map(CreateActivitiesResponse::toDomain)
+            .bind()
+    }
+
+    suspend fun addActivity(activity: FeedActivity): Either<StreamError, FeedActivity> =
+        addActivities(listOf(activity)).map(List<FeedActivity>::first)
 }
