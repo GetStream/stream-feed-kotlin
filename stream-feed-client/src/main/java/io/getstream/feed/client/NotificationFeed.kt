45e4136bab4c23566c979c36350b996b7e65b2cd
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-11-22 18:03:08 +0100

    Create NotificationFeed that extend Feed providing method to get Activities

---
+++ stream-feed-client/src/main/java/io/getstream/feed/client/NotificationFeed.kt
@@ -0,0 +1,53 @@
+package io.getstream.feed.client
+
+import arrow.core.Either
+import arrow.core.computations.either
+import io.getstream.feed.client.internal.api.NotificationFeedApi
+import io.getstream.feed.client.internal.api.models.NotificationsActivitiesGroupResponse
+import io.getstream.feed.client.internal.obtainEntity
+import io.getstream.feed.client.internal.toDomain
+import io.getstream.feed.client.internal.validate
+
+class NotificationFeed internal constructor(
+    private val feedApi: NotificationFeedApi,
+    private val feedID: FeedID,
+) {
+
+    suspend fun getActivities(params: GetActivitiesParams.() -> Unit = {}): Either<StreamError, List<NotificationActivitiesGroup>> =
+        either {
+            val getActivitiesParams = GetActivitiesParams().apply(params).validate().bind()
+            when (getActivitiesParams.enrich) {
+                true -> feedApi.activities(
+                    slug = feedID.slug,
+                    id = feedID.userID,
+                    limit = getActivitiesParams.limit,
+                    offset = getActivitiesParams.offset,
+                    idGreaterThan = getActivitiesParams.idGreaterThan,
+                    idSmallerThan = getActivitiesParams.idSmallerThan,
+                    idGreaterThanOrEqual = getActivitiesParams.idGreaterThanOrEqual,
+                    idSmallerThanOrEqual = getActivitiesParams.idSmallerThanOrEqual,
+                    withRecentReactions = getActivitiesParams.withRecentReactions,
+                    withOwnReactions = getActivitiesParams.withOwnReactions,
+                    withReactionCounts = getActivitiesParams.withReactionCounts,
+                    recentReactionsLimit = getActivitiesParams.recentReactionsLimit,
+                )
+                false -> feedApi.enrichActivities(
+                    slug = feedID.slug,
+                    id = feedID.userID,
+                    limit = getActivitiesParams.limit,
+                    offset = getActivitiesParams.offset,
+                    idGreaterThan = getActivitiesParams.idGreaterThan,
+                    idSmallerThan = getActivitiesParams.idSmallerThan,
+                    idGreaterThanOrEqual = getActivitiesParams.idGreaterThanOrEqual,
+                    idSmallerThanOrEqual = getActivitiesParams.idSmallerThanOrEqual,
+                    withRecentReactions = getActivitiesParams.withRecentReactions,
+                    withOwnReactions = getActivitiesParams.withOwnReactions,
+                    withReactionCounts = getActivitiesParams.withReactionCounts,
+                    recentReactionsLimit = getActivitiesParams.recentReactionsLimit,
+                )
+            }
+                .obtainEntity()
+                .map(NotificationsActivitiesGroupResponse::toDomain)
+                .bind()
+        }
+}
