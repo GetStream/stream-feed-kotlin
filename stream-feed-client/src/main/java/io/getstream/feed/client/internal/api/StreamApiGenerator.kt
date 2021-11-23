fabbdfcef61a9da8dff057ec5b36c71fdab16070
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-11-23 11:26:23 +0100

    Create StreamApiGenerator to build all network components

---
+++ stream-feed-client/src/main/java/io/getstream/feed/client/internal/api/StreamApiGenerator.kt
@@ -0,0 +1,33 @@
+package io.getstream.feed.client.internal.api
+
+import com.moczul.ok2curl.CurlInterceptor
+import io.getstream.feed.client.internal.api.adapters.FeedMoshiConverterFactory
+import io.getstream.feed.client.internal.api.interceptors.ApiKeyInterceptor
+import io.getstream.feed.client.internal.api.interceptors.TokenAuthInterceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+internal class StreamApiGenerator(
+    private val apiKey: String,
+    private val userToken: String,
+) {
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.stream-io-api.com")
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(ApiKeyInterceptor(apiKey))
+                    .addInterceptor(TokenAuthInterceptor { userToken })
+                    .addInterceptor(HttpLoggingInterceptor(::println).setLevel(HttpLoggingInterceptor.Level.BODY))
+                    .addInterceptor(CurlInterceptor(::println))
+                    .build()
+            )
+            .addConverterFactory(FeedMoshiConverterFactory)
+            .build()
+    }
+
+    val flatFeedApi: FlatFeedApi by lazy { retrofit.create(FlatFeedApi::class.java) }
+    val notificationFeedApi: NotificationFeedApi by lazy { retrofit.create(NotificationFeedApi::class.java) }
+    val aggregatedFeedApi: AggregatedFeedApi by lazy { retrofit.create(AggregatedFeedApi::class.java) }
+}
