dde57c7c8a930319ed342e6c4b3a72d8d4aa2569
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-11-23 11:37:58 +0100

    Create StreamClient class

--- stream-feed-client/src/main/java/io/getstream/feed/client/StreamClient.kt
+++ stream-feed-client/src/main/java/io/getstream/feed/client/StreamClient.kt
@@ -1,3 +1,17 @@
 package io.getstream.feed.client
 
-class StreamClient
+import io.getstream.feed.client.internal.api.StreamApiGenerator
+
+public class StreamClient private constructor(private val streamApiGenerator: StreamApiGenerator) {
+
+    fun flatFeed(feedID: FeedID): FlatFeed = FlatFeed(streamApiGenerator.flatFeedApi, feedID = feedID)
+    fun notificationFeed(feedID: FeedID): NotificationFeed = NotificationFeed(streamApiGenerator.notificationFeedApi, feedID = feedID)
+    fun aggregatedFeed(feedID: FeedID): AggregatedFeed = AggregatedFeed(streamApiGenerator.aggregatedFeedApi, feedID = feedID)
+
+    companion object {
+
+        @JvmStatic
+        fun connect(apiKey: String, userToken: String): StreamClient =
+            StreamClient(StreamApiGenerator(apiKey, userToken))
+    }
+}
