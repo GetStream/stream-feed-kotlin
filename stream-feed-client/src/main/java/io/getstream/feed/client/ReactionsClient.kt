61657991abb77d2b061e0414cd777a6c1c12bf5b
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-12-07 13:50:32 +0100

    Create a method to add reactions

--- stream-feed-client/src/main/java/io/getstream/feed/client/ReactionsClient.kt
+++ stream-feed-client/src/main/java/io/getstream/feed/client/ReactionsClient.kt
@@ -1,8 +1,28 @@
 package io.getstream.feed.client
 
+import arrow.core.Either
+import arrow.core.computations.either
 import io.getstream.feed.client.internal.api.ReactionApi
+import io.getstream.feed.client.internal.api.models.ReactionDto
+import io.getstream.feed.client.internal.obtainEntity
+import io.getstream.feed.client.internal.toDTO
+import io.getstream.feed.client.internal.toDomain
+import io.getstream.feed.client.internal.validate
 
 class ReactionsClient internal constructor(
     private val reactionApi: ReactionApi,
     private val userId: String,
-)
+) {
+
+    suspend fun add(reaction: Reaction): Either<StreamError, Reaction> = either {
+        reactionApi.addReaction(
+            reaction
+                .validate()
+                .bind()
+                .toDTO(userId)
+        )
+            .obtainEntity()
+            .map(ReactionDto::toDomain)
+            .bind()
+    }
+}
