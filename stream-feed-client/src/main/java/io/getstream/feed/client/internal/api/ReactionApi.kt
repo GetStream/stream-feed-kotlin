17fd76c447f302882903edaa620a0815a702d228
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-12-09 14:30:35 +0100

    Create and endpoint to filter reactions

--- stream-feed-client/src/main/java/io/getstream/feed/client/internal/api/ReactionApi.kt
+++ stream-feed-client/src/main/java/io/getstream/feed/client/internal/api/ReactionApi.kt
@@ -1,10 +1,14 @@
 package io.getstream.feed.client.internal.api
 
+import io.getstream.feed.client.internal.api.models.FilterReactionsResponse
 import io.getstream.feed.client.internal.api.models.ReactionDto
 import io.getstream.feed.client.internal.api.models.ReactionRequest
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 internal interface ReactionApi {
 
@@ -12,4 +16,17 @@ internal interface ReactionApi {
     suspend fun addReaction(
         @Body reactionRequest: ReactionRequest,
     ): Response<ReactionDto>
+
+    @GET("/api/v1.0/reaction/{lookupAttr}/{lookupValue}/{kind}")
+    suspend fun filterReactions(
+        @Path("lookupAttr") lookupAttr: String,
+        @Path("lookupValue") lookupValue: String,
+        @Path("kind") kind: String?,
+        @Query("limit") limit: Int,
+        @Query("id_gt") idGreaterThan: String?,
+        @Query("id_lt") idSmallerThan: String?,
+        @Query("id_gte") idGreaterThanOrEqual: String?,
+        @Query("id_lte") idSmallerThanOrEqual: String?,
+        @Query("with_activity_data") withActivityData: Boolean?,
+    ): Response<FilterReactionsResponse>
 }
