249369daef1b3566353af1aa47e608450d9904e1
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-12-16 13:40:56 +0100

    Provide UserCLient instance from our StreamClient implementation

--- stream-feed-client/src/main/java/io/getstream/feed/client/StreamClient.kt
+++ stream-feed-client/src/main/java/io/getstream/feed/client/StreamClient.kt
@@ -5,6 +5,7 @@ import io.getstream.feed.client.internal.api.StreamApiGenerator
 public class StreamClient private constructor(private val streamApiGenerator: StreamApiGenerator) {
 
     val activityClient: ActivityClient by lazy { ActivityClient(streamApiGenerator.activityApi) }
+    val userClient: UserClient by lazy { UserClient(streamApiGenerator.userApi) }
     fun flatFeed(feedID: FeedID): FlatFeed = FlatFeed(streamApiGenerator.flatFeedApi, feedID = feedID)
     fun notificationFeed(feedID: FeedID): NotificationFeed = NotificationFeed(streamApiGenerator.notificationFeedApi, feedID = feedID)
     fun aggregatedFeed(feedID: FeedID): AggregatedFeed = AggregatedFeed(streamApiGenerator.aggregatedFeedApi, feedID = feedID)
