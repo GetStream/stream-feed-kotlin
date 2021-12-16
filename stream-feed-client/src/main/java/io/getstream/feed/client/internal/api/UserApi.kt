80e92d12121c575014714100472881e6d8d807e6
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-12-16 12:55:47 +0100

    Create User Endpoints

---
+++ stream-feed-client/src/main/java/io/getstream/feed/client/internal/api/UserApi.kt
@@ -0,0 +1,39 @@
+package io.getstream.feed.client.internal.api
+
+import io.getstream.feed.client.internal.api.models.UpdateUserRequest
+import io.getstream.feed.client.internal.api.models.UserDto
+import io.getstream.feed.client.internal.api.models.UserRequest
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+internal interface UserApi {
+
+    @POST("/api/v1.0/user/")
+    suspend fun addUser(
+        @Query("get_or_create") getOrCreate: Boolean,
+        @Body userRequest: UserRequest,
+    ): Response<UserDto>
+
+    @PUT("/api/v1.0/user/{id}")
+    suspend fun updateUser(
+        @Path("id") userId: String,
+        @Body updateUserRequest: UpdateUserRequest,
+    ): Response<UserDto>
+
+    @GET("/api/v1.0/user/{id}")
+    suspend fun getUser(
+        @Path("id") userId: String,
+        @Query("with_follow_counts") withFollowCounts: Boolean,
+    ): Response<UserDto>
+
+    @DELETE("/api/v1.0/user/{id}")
+    suspend fun deleteUser(
+        @Path("id") userId: String,
+    ): Response<Unit>
+}
